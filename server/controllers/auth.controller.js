20eecb2688dc99f9ef80acca89d1d06d811790ef
ahoang2502 <athoang252@gmail.com>
2024-01-24 12:47:01 +1100

    error-middleware+fn

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,8 +1,9 @@
 import bcryptjs from "bcryptjs";
 
 import User from "../models/user.model.js";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
 	const { username, email, password } = req.body;
 
 	if (
@@ -12,8 +13,9 @@ export const signup = async (req, res) => {
 		username === "" ||
 		email === "" ||
 		password === ""
-	)
-		return res.status(400).json({ message: "All fields are required" });
+	) {
+		next(errorHandler(400, "All fields are required"));
+	}
 
 	const hashedPassword = bcryptjs.hashSync(password, 10);
 
@@ -23,6 +25,6 @@ export const signup = async (req, res) => {
 		await newUser.save();
 		res.json({ message: "OK" });
 	} catch (error) {
-		res.status(500).json({ message: error.message });
+		next(error);
 	}
 };

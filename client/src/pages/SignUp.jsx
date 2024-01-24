52ba3ee808e78fb07806b35c0753b060d6e06d94
ahoang2502 <athoang252@gmail.com>
2024-01-24 19:38:11 +1100

    signup-page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,11 +1,56 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { IoWarning } from "react-icons/io5";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 export const SignUp = () => {
+	const [formData, setFormData] = useState({});
+	const [isLoading, setIsLoading] = useState(false);
+	const [errorMessage, setErrorMessage] = useState(null);
+
+	const navigate = useNavigate();
+
+	const handleChange = (e) => {
+		setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+	};
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+
+		if (!formData.username || !formData.email || !formData.password)
+			return setErrorMessage("Please fill out all fields");
+
+		try {
+			setIsLoading(true);
+			setErrorMessage(null);
+
+			const res = await fetch("/api/auth/signup", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(formData),
+			});
+
+			const data = await res.json();
+
+			if (data.success === false) {
+				setErrorMessage(data.message);
+			}
+
+			if (res.ok) navigate("/sign-in");
+		} catch (error) {
+			setErrorMessage(error.message);
+		} finally {
+			setIsLoading(false);
+		}
+	};
+
 	return (
-		<div className="min-h-screen mt-10 ">
-			<div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+		<div className="min-h-screen mt-20 ">
+			<div className="flex p-3 max-w-lg mx-auto flex-col  md:items-center gap-5">
 				{/* Left-side */}
-				<div className="flex-1">
+				<div className="w-full">
 					<Link to="/" className="font-bold dark:text-white text-4xl">
 						{/* <img src="../assets/logo.svg" /> */}
 						Orbital
@@ -18,34 +63,66 @@ export const SignUp = () => {
 				</div>
 
 				{/* Right-side */}
-				<div className="flex-1">
-					<form className="flex flex-col gap-4">
-						<input
-							id="username"
-							type="text"
-							placeholder="Username"
-							className="rounded-[0.2rem] border border-zinc-300 text-sm ring-pink-300 shadow-sm"
-						/>
+				<div className="w-full">
+					<form onSubmit={handleSubmit} className="flex flex-col gap-4">
+						<div className="flex flex-col gap-1">
+							<label
+								htmlFor="username"
+								className="text-zinc-500 text-xs font-medium"
+							>
+								Username
+							</label>
+							<input
+								id="username"
+								type="text"
+								placeholder="Enter your username..."
+								className="rounded-[0.2rem] border border-zinc-300 text-sm ring-pink-300 shadow-sm"
+								onChange={handleChange}
+							/>
+						</div>
 
-						<input
-							id="email"
-							type="email"
-							placeholder="Email"
-							className="rounded-[0.2rem] border border-zinc-300 text-sm ring-pink-300 shadow-sm"
-						/>
+						<div className="flex flex-col gap-1">
+							<label
+								htmlFor="email"
+								className="text-zinc-500 text-xs font-medium"
+							>
+								Email
+							</label>
+							<input
+								id="email"
+								type="email"
+								placeholder="Enter your email address..."
+								className="rounded-[0.2rem] border border-zinc-300 text-sm ring-pink-300 shadow-sm"
+								onChange={handleChange}
+							/>
+						</div>
 
-						<input
-							id="password"
-							type="password"
-							placeholder="Password"
-							className="rounded-[0.2rem] border border-zinc-300 text-sm ring-pink-300 shadow-sm"
-						/>
+						<div className="flex flex-col gap-1">
+							<label
+								htmlFor="password"
+								className="text-zinc-500 text-xs font-medium"
+							>
+								Password
+							</label>
+							<input
+								id="password"
+								type="password"
+								placeholder="*********"
+								className="rounded-[0.2rem] border border-zinc-300 text-sm ring-pink-300 shadow-sm"
+								onChange={handleChange}
+							/>
+						</div>
 
 						<button
 							type="submit"
-							className="bg-black dark:bg-zinc-100 text-white dark:text-black py-2.5 rounded-[0.2rem] text-sm font-medium hover:bg-black/80 dark:hover:bg-white/80"
+							className="bg-black dark:bg-zinc-100 text-zinc-100 dark:text-black py-2 rounded-[0.2rem] text-sm font-medium hover:bg-black/80 dark:hover:bg-zinc-100/80 flex justify-center items-center"
+							disabled={isLoading}
 						>
-							Sign Up
+							{isLoading ? (
+								<AiOutlineLoading3Quarters className="animate-spin h-5 w-5" />
+							) : (
+								"Sign Up"
+							)}
 						</button>
 					</form>
 
@@ -55,6 +132,15 @@ export const SignUp = () => {
 							Sign In
 						</Link>
 					</div>
+
+					{errorMessage && (
+						<div className="mt-5 flex items-center gap-2 p-2 bg-rose-50 border border-rose-200 rounded-sm">
+							<IoWarning className="text-rose-500" />
+							<p className="text-rose-500 text-sm font-medium">
+								{errorMessage}
+							</p>
+						</div>
+					)}
 				</div>
 			</div>
 		</div>
--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json());
 
 app.listen(3001, () => {
-	console.log("Server is running on port 3000!");
+	console.log("Server is running on port 3001!");
 });
 
 app.use("/api/user", userRoutes);

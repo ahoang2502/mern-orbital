d6971189ce4dd6e108e167ed946cb46f85e47779
ahoang2502 <athoang252@gmail.com>
2024-01-27 13:53:13 +1100

    header-userButton

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,21 @@
-import { Button, Navbar, TextInput } from "flowbite-react";
-import { Link, useLocation } from "react-router-dom";
+import {
+	Avatar,
+	Button,
+	Dropdown,
+	DropdownHeader,
+	DropdownItem,
+	Navbar,
+	TextInput,
+} from "flowbite-react";
 import { AiOutlineSearch } from "react-icons/ai";
 import { FaMoon } from "react-icons/fa";
+import { IoLogOutOutline } from "react-icons/io5";
+import { useSelector } from "react-redux";
+import { Link, useLocation } from "react-router-dom";
 
 export const Header = () => {
 	const path = useLocation().pathname;
-
-  
+	const { currentUser } = useSelector((state) => state.user);
 
 	return (
 		<Navbar className="border-b-2">
@@ -36,14 +45,49 @@ export const Header = () => {
 					<FaMoon size={13} color="#1e293b" />
 				</button>
 
-				<Link to="/sign-in">
-					<Button
-						className="bg-black text-white p-0 hover:bg-black/80"
-						color=""
+				{currentUser ? (
+					<Dropdown
+						arrowIcon={false}
+						inline
+						label={
+							<Avatar
+								alt="user-avatar"
+								img={currentUser.profilePicture}
+								rounded
+								className="h-8 w-10"
+							/>
+						}
 					>
-						Login
-					</Button>
-				</Link>
+						<DropdownHeader>
+							<span className="block text-sm ">@{currentUser.username}</span>
+							<span className="block text-sm font-medium truncate">
+								{currentUser.email}
+							</span>
+						</DropdownHeader>
+
+						<Link to="/dashboard?tab=profile">
+							<DropdownItem>Profile</DropdownItem>
+						</Link>
+
+						<Dropdown.Divider />
+
+						<DropdownItem>
+							<span className="flex justify-between w-full items-center">
+								Logout
+								<IoLogOutOutline className="h-5 w-5" />
+							</span>
+						</DropdownItem>
+					</Dropdown>
+				) : (
+					<Link to="/sign-in">
+						<Button
+							className="bg-black text-white p-0 hover:bg-black/80"
+							color=""
+						>
+							Login
+						</Button>
+					</Link>
+				)}
 
 				<Navbar.Toggle />
 			</div>
